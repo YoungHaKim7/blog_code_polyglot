c545e3dcaa627f14dc0752e83f7e8871208ca6a6
GYoung <ytok1108@aol.com>
2026-01-12 00:44:40 +0900

    Rust array

--- Rust_Lang/004_FP_Functional_Programming/a01_arrays/src/main.rs
+++ Rust_Lang/004_FP_Functional_Programming/a01_arrays/src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     let my_squares = my_arr.map(|c| c * c);
     let my_squares_2 = my_arr.map(squares);
     let my_is_even = my_arr.iter().filter(|c| *c % 2 == 0);
+    let my_arr_filter25 = my_arr.iter().filter_map(|c| Some(*c > 25));
+
     println!("basic list : {my_arr:?}");
     println!("arr idx 2 : {my_idx_2:?}");
     println!("arr map :{my_arr_map_2:?}");
@@ -16,12 +18,13 @@ fn main() {
     println!("arr squares :{my_squares_2:?}");
     println!("arr filter(isEven) :{my_is_even:?}");
     print!("arr filter(isEven) : [");
-    for i in my_is_even {
-        if len() - 1 {
-            print!("{}, ", i);
-        } else {
-            print! {"{}", i};
+    for (idx, i) in my_is_even.enumerate() {
+        if idx > 0 {
+            print!(", ",);
         }
+        print!("{}", i);
     }
     println!("]");
+
+    println!("arr filter > 25 :{my_arr_filter25:?}");
 }

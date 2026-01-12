69b74e80619cd9fab014a3ce97377917a06b56f9
GYoung <ytok1108@aol.com>
2026-01-12 20:32:54 +0900

    array Rust

--- Rust_Lang/004_FP_Functional_Programming/a01_arrays/src/main.rs
+++ Rust_Lang/004_FP_Functional_Programming/a01_arrays/src/main.rs
@@ -4,21 +4,21 @@ fn squares(x: i32) -> i32 {
 
 fn main() {
     let my_arr = [10, 20, 30, 40];
-    let my_idx_2 = my_arr[2];
-    let my_arr_map_2 = my_arr.map(|c| c * 3);
-    let my_squares = my_arr.map(|c| c * c);
-    let my_squares_2 = my_arr.map(squares);
-    let my_is_even = my_arr.iter().filter(|c| *c % 2 == 0);
-    let my_arr_filter25: Vec<i32> = my_arr
-        .iter()
-        .filter_map(|c| if c > &25 { Some(*c) } else { None })
-        .collect();
-
     println!("basic list : {my_arr:?}");
+
+    let my_idx_2 = my_arr[2];
     println!("arr idx 2 : {my_idx_2:?}");
+
+    let my_arr_map_2 = my_arr.map(|c| c * 3);
     println!("arr map :{my_arr_map_2:?}");
+
+    let my_squares = my_arr.map(|c| c * c);
     println!("arr squares :{my_squares:?}");
+
+    let my_squares_2 = my_arr.map(squares);
     println!("arr squares :{my_squares_2:?}");
+
+    let my_is_even = my_arr.iter().filter(|c| *c % 2 == 0);
     println!("arr filter(isEven) :{my_is_even:?}");
     print!("arr filter(isEven) : [");
     for (idx, i) in my_is_even.enumerate() {
@@ -29,5 +29,9 @@ fn main() {
     }
     println!("]");
 
+    let my_arr_filter25: Vec<i32> = my_arr
+        .iter()
+        .filter_map(|c| if c > &25 { Some(*c) } else { None })
+        .collect();
     println!("arr filter > 25 :{my_arr_filter25:?}");
 }

0a52014a4559a0300aaad0d3ed2d9c9aec4634db
GYoung <ytok1108@aol.com>
2026-01-25 23:36:08 +0900

    read file

---
+++ Rust_Lang/ParallelismConcurrency/a05_tokio_hello/src/main.rs
@@ -0,0 +1,23 @@
+async fn async_print(data: Result<Vec<u8>, std::io::Error>) {
+    match data {
+        Ok(contents) => println!("{:?}", contents),
+        Err(e) => eprintln!("Error reading file: {}", e),
+    }
+}
+
+async fn async_print_txt(data: Result<Vec<u8>, std::io::Error>) {
+    match data {
+        Ok(contents) => println!("File contents: {:?}", String::from_utf8(contents)),
+        Err(e) => eprintln!("Error reading file: {}", e),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let data = tokio::fs::read("wow.txt").await;
+    let handle = tokio::spawn(async move {
+        // async_print(data).await;
+        async_print_txt(data).await;
+    });
+    handle.await.unwrap();
+}

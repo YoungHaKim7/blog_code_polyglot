e2574a40f81b0e4e684d5e505742e46ac7751767
GYoung <ytok1108@aol.com>
2026-01-27 00:49:08 +0900

    string concat

--- 005_std_Cooking_Rust/chapter001/src/bin/format.rs
+++ 005_std_Cooking_Rust/chapter001/src/bin/format.rs
@@ -1,4 +1,11 @@
 fn main() {
     let my_num = format!("나의 좋아하는 숫자는 : {}", 42);
     println!("출력 해보자 : {my_num}");
+
+    let color = "black";
+
+    let hello = "hello ";
+    let world = "world ";
+
+    println!("내가 좋아하는 색깔은 {}, {}, {}", color, hello, world);
 }

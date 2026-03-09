eda831dedce39cf8fbc1c93845b0394b6810f90c
GYoung <ytok1108@aol.com>
2026-03-09 23:28:46 +0900

    binary_gray_code_ xor

--- 008_Rust_Kernel/Binary_Hex_Patters/gray_to_binary_xor/src/main.rs
+++ 008_Rust_Kernel/Binary_Hex_Patters/gray_to_binary_xor/src/main.rs
@@ -7,10 +7,21 @@ fn gray_to_binary(mut b: u32) -> u32 {
     b
 }
 
+fn gray_to_binary02(mut x: u32) -> u32 {
+    let mut shift = 1;
+    while shift < 32 {
+        x ^= x >> shift;
+        shift <<= 1;
+    }
+    x
+}
+
 fn main() {
     let gray: u32 = 0b1110; // Gray code
     let binary = gray_to_binary(gray);
+    let binary02 = gray_to_binary02(gray);
 
     println!("gray   : {:04b}", gray);
     println!("binary : {:04b}", binary);
+    println!("binary : {:04b}", binary02);
 }

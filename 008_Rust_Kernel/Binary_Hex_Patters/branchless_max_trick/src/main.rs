9279918ee1c68c023a190440398df1ee5838dfef
GYoung <ytok1108@aol.com>
2026-03-11 21:35:20 +0900

    fast_popcount_step

---
+++ 008_Rust_Kernel/Binary_Hex_Patters/branchless_max_trick/src/main.rs
@@ -0,0 +1,23 @@
+// 10   // 0000 1010
+// 6    // 0000 0110
+// x ^ y // 0000 1100    // 10 dec
+
+fn branchless_max(x: i32, y: i32) -> i32 {
+    x ^ ((x ^ y) & -((x < y) as i32))
+}
+
+fn main() {
+    let x = 10;
+    let y = 6;
+
+    let max = branchless_max(x, y);
+
+    println!("x   : {}", x);
+    println!("y   : {}", y);
+    println!("max : {}", max);
+
+    let test_eval = x < y as i32;
+    println!("10 < 6 as i32 : {}", test_eval);
+    println!("-(10 < 6 as i32) : {}", -(test_eval as i32));
+    // println!("10 < 6 as i32 : {:08b}", test_eval);
+}
